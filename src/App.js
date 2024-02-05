ca98e61d961632bcfe601ceaf24cad3273a3dce0
mona9855 <mona.magdy2012912012911@gmail.com>
2024-02-05 13:09:38 +0200

    pushing project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import CanvasModel from './canvas/index';
+import Customizer from './pages/Customizer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className='app transition-all ease-in'>
+      <Home />
+      <CanvasModel />
+      <Customizer />
+    </main>
   );
 }
 
---
+++ src/config/constants.js
@@ -0,0 +1,35 @@
+import { swatch, fileIcon, logoShirt, stylishShirt } from "../assets";
+
+export const EditorTabs = [
+  {
+    name: "colorpicker",
+    icon: swatch,
+  },
+  {
+    name: "filepicker",
+    icon: fileIcon,
+  },
+  
+];
+
+export const FilterTabs = [
+  {
+    name: "logoShirt",
+    icon: logoShirt,
+  },
+  {
+    name: "stylishShirt",
+    icon: stylishShirt,
+  },
+];
+
+export const DecalTypes = {
+  logo: {
+    stateProperty: "logoDecal",
+    filterTab: "logoShirt",
+  },
+  full: {
+    stateProperty: "fullDecal",
+    filterTab: "stylishShirt",
+  },
+};
